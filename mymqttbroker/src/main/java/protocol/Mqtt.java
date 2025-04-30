87013a463698f476f504e3f47efa4afec957faa8
ckab04 <ciceronkapenabm@gmail.com>
2025-04-30 23:02:25 +0200

    Mqtt  interface

--- mymqttbroker/src/main/java/protocol/Mqtt.java
+++ mymqttbroker/src/main/java/protocol/Mqtt.java
@@ -1,4 +1,8 @@
 package protocol;
 
 public interface Mqtt {
+    int mqttEncodeLength(String value, int type);
+    long mqttDecodeLength(String value, int type);
+    int unpackMqttPacket(String value, MqttPacket packet);
+    String packMqttPacket(MqttPacket packet, int type);
 }
