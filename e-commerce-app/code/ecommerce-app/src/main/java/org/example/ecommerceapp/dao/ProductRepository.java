6acdc1754ae6e33208055a0c238698376093df51
ckab04 <ciceronkapenabm@gmail.com>
2025-07-13 14:27:47 +0200

    Release 1.0 completed

--- e-commerce-app/code/ecommerce-app/src/main/java/org/example/ecommerceapp/dao/ProductRepository.java
+++ e-commerce-app/code/ecommerce-app/src/main/java/org/example/ecommerceapp/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package org.example.ecommerceapp.dao;
 
 import org.example.ecommerceapp.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
