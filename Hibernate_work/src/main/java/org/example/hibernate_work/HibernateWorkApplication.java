61a3d66c8358e59c27160b4adb0709ca07662fcc
ckab04 <ciceronkapenabm@gmail.com>
2026-04-24 22:54:49 +0200

    Set up

---
+++ Hibernate_work/src/main/java/org/example/hibernate_work/HibernateWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.hibernate_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateWorkApplication.class, args);
+    }
+
+}
