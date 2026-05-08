6570bb1ad291f6ee1035a947a998109cfcb325d9
ckab04 <ciceronkapenabm@gmail.com>
2026-05-08 02:44:33 +0200

    Project entity

--- Hibernate_work/src/main/java/org/example/hibernate_work/entities/Project.java
+++ Hibernate_work/src/main/java/org/example/hibernate_work/entities/Project.java
@@ -1,4 +1,31 @@
 package org.example.hibernate_work.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "projects")
+    private Set<Department> departments = new HashSet<>();
+
+    public Project(String name){
+        this.name = name;
+        this.departments = new HashSet<>();
+    }
+
 }
